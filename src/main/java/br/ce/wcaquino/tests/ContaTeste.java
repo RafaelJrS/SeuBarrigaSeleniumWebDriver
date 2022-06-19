768efeb093b0f05030a2e9b82d53bc0f2056a76f
RafaelJrS <silvarafaeljr@gmail.com>
2022-06-19 18:16:53 -0300

    Teste Login

---
+++ src/main/java/br/ce/wcaquino/tests/ContaTeste.java
@@ -0,0 +1,15 @@
+package br.ce.wcaquino.tests;
+
+import org.junit.Test;
+
+import br.ce.wcaquino.core.BaseTest;
+
+public class ContaTeste extends BaseTest {
+	
+	@Test
+	public void test() {
+		
+	}
+	
+
+}
